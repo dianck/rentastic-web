c43003024e5f5f8702d21c4a7a0a5942b65bfcb0
dianck2002 <dianck2002@gmail.com>
2025-07-11 08:44:01 +0700

    logo

--- src/components/verify.tsx
+++ src/components/verify.tsx
@@ -44,13 +44,15 @@ export default function VerifyPage({ token }: { token: string }) {
     <div className="flex w-full h-screen justify-center items-center perspective-1000 bg-white">
       <div className="text-black flex gap-2 animate-bounce">
         <Image
-          alt="logo-eventique"
-          src={"/logo4.png"}
+          alt="logo"
+          src={`/${(process.env.NEXT_PUBLIC_LOGO || "fallback-logo.png").replace(/^\/+/, "")}`}
           width={100}
           height={100}
           className="h-8 w-8"
           priority
         />
+
+
         <span className="self-center text-2xl font-semibold whitespace-nowrap animate-pulse">
           {msg}
         </span>
