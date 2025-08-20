ce181b88dbf8edf5d92246f406b1517eab4339ff
dianck2002 <dianck2002@gmail.com>
2025-08-20 16:50:27 +0700

    recommit

--- src/app/(home)/reg-property/page.tsx
+++ src/app/(home)/reg-property/page.tsx
@@ -1,18 +1,43 @@
+"use client";
+
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import EditPropertyPage from "@/components/form/property";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function Home() {
+export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/"); // redirect kalau belum login
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return (
+      <main className="flex-grow flex items-center justify-center">
+        <p className="text-gray-600">Loading...</p>
+      </main>
+    );
+  }
 
   return (
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-            <div className="container mx-auto  items-center justify-between px-4 md:px-6">
-                <EditPropertyPage />
+    <div className="flex flex-col min-h-screen">
+      {session?.user?.email ? (
+        <>
+          <Header />
+          <main className="flex-grow">
+            <div className="container mx-auto items-center justify-between px-4 md:px-6">
+              <EditPropertyPage />
             </div>
-        </main>
-        <Footer />
-      </div>
+          </main>
+          <Footer />
+        </>
+      ) : null}
+    </div>
   );
 }
